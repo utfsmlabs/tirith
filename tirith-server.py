7e6acb622b31a303f8e3a27044541aa93a672848
René Mujica Moreau <rene.mujica.m@gmail.com>
2016-04-29 19:28:04 -0300

    triggering on disconnection

--- tirith-server.py
+++ tirith-server.py
@@ -1,16 +1,20 @@
 import asyncio
 import websockets
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print("< {}".format(name))
+clients = set()
+async def handler(websocket, path):
+    while True:
+        try:
+            message = await websocket.recv()
+            clients.add(websocket.remote_address[0])
+            print(','.join(clients))
+        except websockets.exceptions.ConnectionClosed:
+            print("connection closed")
+            clients.remove(websocket.remote_address[0])
+            break
+    print(','.join(clients))
     
-    greeting = "Hello {}!".format(name)
-    await websocket.send(greeting)
-    
-    print("> {}".format(greeting))
-    
-start_server = websockets.serve(hello, 'localhost', 6969)
+start_server = websockets.serve(handler, 'localhost', 6969)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
